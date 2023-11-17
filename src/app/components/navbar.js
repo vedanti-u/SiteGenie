c6e3703cd8a50ee2ace5eef8f42804624ed4e996
VarunGyanchandani <varungyanchandani74306@gmail.com>
2023-11-17 13:21:27 +0530

    Added login and sign up pages in their respective folders

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -12,6 +12,12 @@ const Navbar = () => {
         <a href="../about" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
       </li>
       <li>
+        <a href="../signup" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">SignUp</a>
+      </li>
+      <li>
+        <a href="../login" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Login</a>
+      </li>
+      <li>
         <a href="../chatbot" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">ChatBot</a>
       </li>
       <li>
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-
 //import Image from 'next/image'
 
 //import Chatbot from "./chatbot/chatbot";
