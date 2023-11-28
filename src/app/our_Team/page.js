94995450e6884d7f35ff9f526da69ac45d50b79e
VarunGyanchandani <varungyanchandani74306@gmail.com>
2023-11-29 00:47:24 +0530

    Modified landing and signin page

--- src/app/our_Team/page.js
+++ src/app/our_Team/page.js
@@ -1,18 +1,17 @@
 const ourTeam = () => {
     const people = [
         {
-          name: 'Lindsay Walton',
-          role: 'Front-end Developer',
-          imageUrl:
-            'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+          name: 'Vedanti',
+          role: 'Backend',
+          imageUrl:''
         },
         {
-            name: 'Vojas',
-            role: 'Frontend',
-            imageUrl:''
+          name: 'Vojas',
+          role: 'Frontend',
+          imageUrl:''
         },
         {
-          name: 'Bhale',
+          name: 'Vedant',
           role: 'Integration',
           imageUrl:''
         },
--- src/app/signin/success.js
+++ src/app/signin/success.js
@@ -4,30 +4,28 @@ import { useRouter } from 'next/navigation';
 import { createClient } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const supabase = createClient('https://jphemcuwfpxnynveilja.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwaGVtY3V3ZnB4bnludmVpbGphIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk5NTMxMTUsImV4cCI6MjAxNTUyOTExNX0.y8HPHiQHoXP0iIWnrgxptJ1X4uvw-ycRmiFNHnndSUM')
 
-function Login () {
+function Success() {
+    const [user, SetUser]=useState({});
+    const router=useRouter();
 
-    // const router=useRouter();
-    // supabase.auth.onAuthStateChange(async (event) => {
-    //   if(event !== "SIGNED_OUT"){
-    //       router.push("/menu");
-    //   }else{
-    //       router.push("/");
-    //   }
-    // })
-    
-    return (
-    
-            <Auth
-                supabaseClient={supabase}
-                appearance={{ theme: ThemeSupa }}
-                theme="dark"
-                providers={['google']}
-            />
+    useEffect(() => {
+        async function getUserData() {
+            await supabase.auth.getUser().then((value) => {
+                if(value.data?.user){
+                    SetUser(value.data.user);
+                }
+            })
+        }
+        getUserData();
+    }, []);
+
+    return(
+        <div>
+            <h1>Success</h1>
+        </div>
     )
 }
-
-export default Login;
