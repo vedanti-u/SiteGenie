58228d86232b46424cb687c5e130e917037fbfe5
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2024-05-20 01:32:17 +0530

    finalising user-dashboard

--- src/app/user-dashboard/page.js
+++ src/app/user-dashboard/page.js
@@ -6,6 +6,8 @@ import { useState, useRef, useEffect, useMemo } from "react";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import Image from "next/image";
 import React from "react";
+import { IconSignatureOff } from "@tabler/icons-react";
+import { LogOut } from "lucide-react";
 
 const create = {
   tiers: [
@@ -102,18 +104,8 @@ export default function Home() {
                       className="flex justify-between items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 w-full text-left"
                     >
                       <span>Sign Out</span>
-                      <svg
-                        width={25}
-                        height={25}
-                        viewBox="0 0 15 15"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M13.5 7.5L10.5 10.75M13.5 7.5L10.5 4.5M13.5 7.5L4 7.5M8 13.5H1.5L1.5 1.5L8 1.5"
-                          stroke="gray"
-                        />
-                      </svg>
+
+                      <LogOut />
                     </button>
                   </div>
                 )}
