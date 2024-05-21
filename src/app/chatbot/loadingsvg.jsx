ffd87a832dcc096ae6b2af4c247af9a594dd5919
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2024-05-22 03:21:14 +0530

    Chatbot layout fixed with proper rendering

--- src/app/chatbot/loadingsvg.jsx
+++ src/app/chatbot/loadingsvg.jsx
@@ -4,8 +4,8 @@ function Loadingsvg() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="100"
-      height="100"
+      width="80"
+      height="80"
       display="block"
       preserveAspectRatio="xMidYMid"
       viewBox="0 0 100 100"
