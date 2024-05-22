7145ccb3bac6f6fbff450d895bfdb4b1312efb42
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2024-05-23 02:38:52 +0530

    modified team section

--- src/components/teamsection.js
+++ src/components/teamsection.js
@@ -1,3 +1,4 @@
+import { User2Icon } from "lucide-react";
 import React from "react";
 
 const Teamsection = () => {
@@ -6,81 +7,43 @@ const Teamsection = () => {
       <section class="py-24 ">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div class="mb-12">
-            <h2 class="font-manrope text-5xl text-center font-bold text-gray-900 ">
-              Our team{" "}
-            </h2>
+            <h2 class="text-5xl text-center text-gray-900 ">Our team </h2>
           </div>
-          <div class="grid grid-cols-1 min-[500px]:grid-cols-2 md:grid-cols-6 lg:grid-cols-5 gap-8 max-w-xl mx-auto md:max-w-3xl lg:max-w-full">
-            <div class="block group md:col-span-2 lg:col-span-1 ">
+          <div class="grid grid-cols-1 min-[500px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8 max-w-xl mx-auto md:max-w-3xl lg:max-w-4xl">
+            <div class="block group md:col-span-1 lg:col-span-1">
               <div class="relative mb-6">
-                <img
-                  src="https://pagedone.io/asset/uploads/1696238374.png"
-                  alt="Antonio image"
-                  class="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600"
-                />
+                <User2Icon className="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600" />
               </div>
-              <h4 class="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
-                Antonio Roberto{" "}
-              </h4>
-              <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
-                Founder
-              </span>
-            </div>
-            <div class="block group md:col-span-2 lg:col-span-1 ">
-              <div class="relative mb-6">
-                <img
-                  src="https://pagedone.io/asset/uploads/1696238396.png"
-                  alt="Patricia image"
-                  class="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600"
-                />
-              </div>
-              <h4 class="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
-                Patricia Angely{" "}
+              <h4 class="text-lg font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
+                Vedanti Udapure
               </h4>
               <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
                 Co-Founder
               </span>
             </div>
-            <div class="group group md:col-span-2 lg:col-span-1">
+
+            <div class="block group md:col-span-1 lg:col-span-1">
               <div class="relative mb-6">
-                <img
-                  src="	https://pagedone.io/asset/uploads/1696238411.png"
-                  alt="Jerom image"
-                  class="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600"
-                />
+                <div class="relative mb-6">
+                  <User2Icon className="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600" />
+                </div>
               </div>
-              <h4 class="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
-                Jerom Bell{" "}
+              <h4 class="text-lg font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
+                Vedant Bhalerao
               </h4>
               <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
                 Chairman
               </span>
             </div>
-            <div class="block group md:col-span-2 lg:col-span-1 md:col-start-2 lg:col-start-4">
+
+            <div class="block group md:col-span-1 lg:col-span-1">
               <div class="relative mb-6">
-                <img
-                  src="	https://pagedone.io/asset/uploads/1696238425.png"
-                  alt="Yasmine image"
-                  class="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600"
-                />
+                <div class="relative mb-6">
+                  <User2Icon className="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600" />
+                </div>
               </div>
-              <h4 class="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
-                Yasmine Tano{" "}
-              </h4>
-              <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
-                CEO
-              </span>
-            </div>
-            <div class="block group min-[500px]:col-span-2 mx-auto md:col-span-2 lg:col-span-1 ">
-              <div class="relative mb-6">
-                <img
-                  src="https://pagedone.io/asset/uploads/1696238446.png"
-                  alt="Martin image"
-                  class="w-40 h-40 rounded-full mx-auto transition-all duration-500 object-cover border border-solid border-transparent group-hover:border-indigo-600"
-                />
-              </div>
-              <h4 class="text-xl font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
-                Martin Darbys
+              <h4 class="text-lg font-semibold text-gray-900 mb-2 capitalize text-center transition-all duration-500 group-hover:text-indigo-600">
+                Vojas Gonnade
               </h4>
               <span class="text-gray-500 text-center block transition-all duration-500 group-hover:text-gray-900">
                 Product Manager
